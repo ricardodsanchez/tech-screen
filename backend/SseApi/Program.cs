3265f05548cf54a68f044056ddcd74777a402443
Ricardo Sanchez <rsanchez2@paylocity.com>
2025-09-14 22:49:20 -0700

    Updates

--- backend/SseApi/Program.cs
+++ backend/SseApi/Program.cs
@@ -9,10 +9,6 @@ builder.Services.AddSingleton<IAccountStore, InMemoryAccountStore>();
 builder.Services.AddSingleton<ITransferStore, InMemoryTransferStore>();
 builder.Services.AddSingleton<TransferService>();
 
-// Intentional omission: builder.Services.AddControllers();
-
-builder.Services.AddControllers();
-
 builder.Services.AddCors(options =>
 {
 	options.AddDefaultPolicy(policy =>
@@ -26,13 +22,10 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Intentional omission: app.UseCors();
-app.UseCors();
-
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapControllers(); // Will not map without AddControllers (intentional for discussion)
+app.MapControllers();
 app.MapGet("/health", () => Results.Ok(new { ok = true }));
 
 app.Run("http://localhost:5178");
